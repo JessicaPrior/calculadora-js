29ecd4be78eb24de8066ddf4b84a6f5b5dcec126
JessicaPrior <jsscprior@gmail.com>
2021-03-10 18:12:28 -0300

    Refatoração do objeto calculadora

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,34 +1,61 @@
 var Calculadora = {
+    ADICAO: '+',
+    SUBTRACAO: '-',
+    DIVISAO: '/',
+    MULTIPLICACAO: '*',
+
     somar: function (n1, n2) {
-        if (isNaN(n1) || isNaN(n2)) {
-            return 0
-        }
-
-        n1 = parseFloat(n1)
-        n2 = parseFloat(n2)
-
-        return n1 + n2
+        
+        return Calculadora.calcular(n1, n2,
+            Calculadora.ADICAO)
     },
     subtrair: function (n1, n2) {
-        if (isNaN(n1) || isNaN(n2)) {
-            return 0
-        }
-        return n1 - n2
+
+        return Calculadora.calcular(n1, n2, 
+            Calculadora.SUBTRACAO)
     },
     dividir: function (n1, n2) {
         if (n2 == 0) {
             return 'erro'
         }
-        if (isNaN(n1) || isNaN(n2)) {
-            return 0
-        }
-        return n1 / n2
+
+        return Calculadora.calcular(n1, n2,
+            Calculadora.DIVISAO)
     },
     multiplicar: function (n1, n2) {
+
+        return Calculadora.calcular(n1, n2, 
+            Calculadora.MULTIPLICACAO)
+    },
+
+    calcular: function (n1, n2, operacao) {
+        var result = 0
+
+        n1 = parseFloat(n1)
+        n2 = parseFloat(n2)
+
         if (isNaN(n1) || isNaN(n2)) {
             return 0
         }
-        return n1 * n2
+
+        switch (operacao) {
+            case Calculadora.ADICAO:
+                result = n1 + n2
+                break;
+            case Calculadora.SUBTRACAO:
+                result = n1 - n2
+                break;
+            case Calculadora.DIVISAO:
+                result = n1 / n2
+                break;
+            case Calculadora.MULTIPLICACAO:
+                result = n1 * n2
+                break;
+            default:
+                result = 0
+                break;
+        }
+        return result
     }
 }
 
